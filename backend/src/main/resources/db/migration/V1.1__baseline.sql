a243b8417c7e61028e2e03e9ed380e0ac0b3fa8a
traderres <traderres@yahoo.com>
2021-04-04 21:37:19 -0400

    Added flyway to the project

---
+++ backend/src/main/resources/db/migration/V1.1__baseline.sql
@@ -0,0 +1,6 @@
+--------------------------------------------------------------
+-- Filename:  V1.1__baseline.sql
+--------------------------------------------------------------
+
+-- Create a sequence called "seq_table_ids"
+Create sequence seq_table_ids increment by 1 START WITH 1000;
