4c0f249aee4f3f7e39c454c57690de36e23f3128
traderres <traderres@yahoo.com>
2021-04-04 21:55:18 -0400

    Added a back-end LookupController and lookupService that returns lookup information from the database.

---
+++ backend/src/main/java/com/lessons/controllers/LookupController.java
@@ -0,0 +1,54 @@
+package com.lessons.controllers;
+
+import com.lessons.models.LookupDTO;
+import com.lessons.services.LookupService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller("com.lessons.controllers.LookupController")
+public class LookupController {
+    private static final Logger logger = LoggerFactory.getLogger(LookupController.class);
+
+    @Resource
+    private LookupService lookupService;
+
+
+    /**
+     * Lookup REST endpoints
+     *   GET  /api/lookups/{lookupType}/{orderBy}
+     *   GET  /api/lookups/{lookupType}
+     * @param aLookupType holds the required lookup type
+     * @param aOrderBy    holds the optional orderby Field
+     * @return a list of LookupDTO objects and a 200 status code
+     */
+    @RequestMapping(value = {"/api/lookups/{lookupType}/{orderBy}",
+            "/api/lookups/{lookupType}"}, method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> getLookupsWithType(@PathVariable(name="lookupType")               String aLookupType,
+                                                @PathVariable(name="orderBy", required=false)  String aOrderBy) {
+        logger.debug("getLookupsWithType() started.   aLookupType={}", aLookupType);
+
+        if (! lookupService.doesLookupTypeExist(aLookupType) ) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body("The pass-in lookup type does not exist: " + aLookupType);
+        }
+
+        List<LookupDTO> lookupDTOS = lookupService.getLookupsWithType(aLookupType, aOrderBy);
+
+        // Return the list of LookupDTO objects back to the front-end
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(lookupDTOS);
+    }
+}
---
+++ backend/src/main/java/com/lessons/services/LookupService.java
@@ -0,0 +1,66 @@
+
+package com.lessons.services;
+
+import com.lessons.models.LookupDTO;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.util.List;
+
+@Service("com.lessons.services.LookupService")
+public class LookupService {
+    private static final Logger logger = LoggerFactory.getLogger(LookupService.class);
+
+    @Resource
+    private DataSource dataSource;
+
+
+    /**
+     * @param aLookupType holds the name of the lookup type
+     * @return true if found in the lookup_type table.  False otherwise
+     */
+    public boolean doesLookupTypeExist(String aLookupType) {
+        // Construct the sql to see if this lookup type is found in the lookup_type table
+        String sql = "Select id from lookup_type where name=?";
+
+        // Execute the SQL
+        JdbcTemplate jt = new JdbcTemplate(this.dataSource);
+        SqlRowSet rs = jt.queryForRowSet(sql, aLookupType);
+
+        // Returns true if the lookup name is found, false otherwise
+        return rs.next();
+    }
+
+    /**
+     * @param aLookupType holds a string with a lookup type name
+     * @return list of LookupDTO objects that hold information about all lookups with this lookup type name
+     */
+    public List<LookupDTO> getLookupsWithType(String aLookupType, String aOrderBy) {
+        String sql = "select l.id, l.name\n" +
+                "from lookup l\n" +
+                "join lookup_type lt on (lt.id=l.lookup_type)\n" +
+                "where lt.name=? ";
+
+        if (StringUtils.isNotEmpty(aOrderBy)) {
+            // Append the order by string
+            sql = sql + "order by " + aOrderBy;
+        }
+
+        // Execute the SQL, generating a list of LookupDTO objects
+        // NOTE:  If no lookup records are found, then returns an empty list
+        BeanPropertyRowMapper rowMapper = new BeanPropertyRowMapper(LookupDTO.class);
+        JdbcTemplate jt = new JdbcTemplate(this.dataSource);
+        List<LookupDTO> lookups = jt.query(sql, rowMapper, aLookupType);
+
+        // Return the list of LookupDTO objects (or empty list)
+        return lookups;
+    }
+}
+
