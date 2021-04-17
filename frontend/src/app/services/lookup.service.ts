f6e439c977758f8565dc8c5732e72eda2feceff9
Adam Resnick <aresnick@rbr-tech.com>
2021-04-17 18:09:05 -0400

    Finished coding the lesson.

--- frontend/src/app/services/lookup.service.ts
+++ frontend/src/app/services/lookup.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
-import {Observable} from "rxjs";
+import {EMPTY, Observable} from "rxjs";
 import {LookupDTO} from "../models/lookup-dto";
+import {catchError, shareReplay} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LookupService {
+
+  // The cache holds key=type value=list of LookupDTO objects
+  private cache: any = {};
 
   constructor(private httpClient: HttpClient) { }
 
@@ -16,9 +20,22 @@ export class LookupService {
    */
   public getLookupWithType(aType: string): Observable<LookupDTO[]>  {
 
+    if (this.cache[aType]) {
+      // This observable is in the cache.  So, return it from the cache
+      return this.cache[aType];
+    }
+
     const restUrl = environment.baseUrl + '/api/lookups/' + aType;
 
-    return this.httpClient.get <LookupDTO[]>(restUrl);
+    this.cache[aType] = this.httpClient.get <LookupDTO[]>(restUrl).pipe(
+      shareReplay(1),
+      catchError(err => {
+        console.error('There was an error getting lookup value ' + aType + '.  Error is ', err);
+        delete this.cache[aType];
+        return EMPTY;
+      }));
+
+    return this.cache[aType];
   }
 
 
@@ -26,9 +43,25 @@ export class LookupService {
    * Return a sorted list of LookupDTO objects that correspond to the passed-in type name -- e.g, 'priority'
    */
   public getLookupWithTypeAndOrder(aType: string, aOrderBy: string): Observable<LookupDTO[]>  {
+    let cacheKey = aType + ',' + aOrderBy;
+
+    if (this.cache[cacheKey]) {
+      // This observable is in the cache.  So, return it from the cache
+      return this.cache[cacheKey];
+    }
 
     const restUrl = environment.baseUrl + '/api/lookups/' + aType + '/' + aOrderBy;
 
-    return this.httpClient.get <LookupDTO[]>(restUrl);
+    // Invoke the REST call and add this observable to the cache.
+    this.cache[cacheKey] = this.httpClient.get <LookupDTO[]>(restUrl).pipe(
+      shareReplay(1),
+      catchError(err => {
+        console.error('There was an error getting lookup value ' + aType + '.  Error is ', err);
+        delete this.cache[aType];
+        return EMPTY;
+      }));
+
+    return this.cache[cacheKey];
   }
+
 }
