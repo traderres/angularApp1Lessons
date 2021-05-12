af65ee819adfb3624558f134e646ef827feb7067
Adam Resnick <aresnick@rbr-tech.com>
2021-05-12 17:29:09 -0400

    Finished coding the lesson

--- frontend/src/app/analytics/dashboard/dashboard.component.ts
+++ frontend/src/app/analytics/dashboard/dashboard.component.ts
@@ -24,6 +24,8 @@ HC_offlineExport(Highcharts);
 import HC_drillDown from "highcharts/modules/drilldown";
 import {Chart} from "highcharts";
 import {TileSizeDTO} from "../../models/tile-size-dto";
+import {DashboardService} from "../../services/dashboard.service";
+import {DashboardDataDTO} from "../../models/dashboard-data-dto";
 HC_drillDown(Highcharts);
 
 @Component({
@@ -200,7 +202,8 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
     ];
 
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver,
+              private dashboardService: DashboardService) { }
 
 
   public ngOnInit(): void {
@@ -252,121 +255,24 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
   public reloadData(): void {
     this.dataIsLoading = true;
 
-    // Update chart 1 with hard-coded data
-    this.chartOptions1.series[0].data = [
-      {
-        name: "Item 3",
-        y: 989
-      },
-      {
-        name: "Item 3R",
-        y: 249
-      },
-      {
-        name: "Item 5",
-        y: 1035
-      },
-      {
-        name: "Item 5R",
-        y: 324
-      }
-    ];
-    Highcharts.chart('chart1', this.chartOptions1);
+    this.dashboardService.getAllChartData().subscribe( (aData: DashboardDataDTO) => {
+      // The REST call came back with data
 
+      // Set the data for chart 1 and *render* chart 1
+      this.chartOptions1.series[0].data = aData.chartData1;
+      Highcharts.chart('chart1', this.chartOptions1);
 
-    // Update chart 2 with hard-coded data
-    this.chartOptions2.series =  [
-      {
-        name: 'T3',
-        legendIndex: 1,
-        data: [300, 5500, 1800, 1600, 1200, 1500, 1000, 800, 500, 400, 1000]
-      },
-      {
-        name: 'T3R',
-        legendIndex: 2,
-        data: [2, 2, 100, 2, 1]
-      },
-      {
-        name: 'T5',
-        legendIndex: 3,
-        data: [25, 500, 551, 600, 400, 300, 200, 500, 100, 100, 1200]
-      },
-      {
-        name: 'T5R',
-        legendIndex: 4,
-        data: [200, 190, 190, 100, 50, 12, 37, 42, 98, 50, 600]
-      }
-    ];
-    Highcharts.chart('chart2', this.chartOptions2);
+      this.chartOptions2.series = aData.chartData2;
+      Highcharts.chart('chart2', this.chartOptions2);
 
-    // Update chart 3 with hard-coded data
-    this.chartOptions3.series = [{
-      name: 'T3',
-      data: [
-        [Date.UTC(2019, 6, 1), 110],
-        [Date.UTC(2019, 7, 1), 145],
-        [Date.UTC(2019, 8, 1), 135],
-        [Date.UTC(2019, 9, 1), 140],
-        [Date.UTC(2019, 10, 1), 100],
-        [Date.UTC(2019, 11, 1), 110],
-        [Date.UTC(2019, 12, 1), 100],
-        [Date.UTC(2020, 1, 1), 85],
-        [Date.UTC(2020, 2, 1), 70],
-        [Date.UTC(2020, 3, 1), 65],
-        [Date.UTC(2020, 4, 1), 60],
-        [Date.UTC(2020, 5, 1), 60]
+      this.chartOptions3.series = aData.chartData3;
+      Highcharts.chart('chart3', this.chartOptions3);
 
-      ]
-    }, {
-      name: 'T3R',
-      data: [
-        [Date.UTC(2019, 6, 1), 175],
-        [Date.UTC(2019, 7, 1), 155],
-        [Date.UTC(2019, 8, 1), 100],
-        [Date.UTC(2019, 9, 1), 115],
-        [Date.UTC(2019, 10, 1), 87],
-        [Date.UTC(2019, 11, 1), 90],
-        [Date.UTC(2019, 12, 1), 88],
-        [Date.UTC(2020, 1, 1), 85],
-        [Date.UTC(2020, 2, 1), 86],
-        [Date.UTC(2020, 3, 1), 75],
-        [Date.UTC(2020, 4, 1), 60],
-        [Date.UTC(2020, 5, 1), 45]
-      ]
-    }, {
-      name: 'T5',
-      data: [
-        [Date.UTC(2019, 6, 1), 230],
-        [Date.UTC(2019, 7, 1), 225],
-        [Date.UTC(2019, 8, 1), 205],
-        [Date.UTC(2019, 9, 1), 210],
-        [Date.UTC(2019, 10, 1), 212],
-        [Date.UTC(2019, 11, 1), 185],
-        [Date.UTC(2019, 12, 1), 187],
-        [Date.UTC(2020, 1, 1), 150],
-        [Date.UTC(2020, 2, 1), 105],
-        [Date.UTC(2020, 3, 1), 85],
-        [Date.UTC(2020, 4, 1), 85],
-        [Date.UTC(2020, 5, 1), 70]
-      ]
-    }, {
-      name: 'T5R',
-      data: [
-        [Date.UTC(2019, 6, 1), 240],
-        [Date.UTC(2019, 7, 1), 238],
-        [Date.UTC(2019, 8, 1), 205],
-        [Date.UTC(2019, 9, 1), 200],
-        [Date.UTC(2019, 10, 1), 160],
-        [Date.UTC(2019, 11, 1), 155],
-        [Date.UTC(2019, 12, 1), 148],
-        [Date.UTC(2020, 1, 1), 140],
-        [Date.UTC(2020, 2, 1), 120],
-        [Date.UTC(2020, 3, 1), 85],
-        [Date.UTC(2020, 4, 1), 75],
-        [Date.UTC(2020, 5, 1), 125]
-      ]
-    }];
-    Highcharts.chart('chart3', this.chartOptions3);
+    }).add(  () => {
+      // REST call finally block
+      this.dataIsLoading = false;
+    });
+
 
     // Redraw all of the charts on this page (so they fit perfectly within the mat-card tags
     Highcharts.charts.forEach(function (chart: Chart | undefined) {
---
+++ frontend/src/app/models/dashboard-data-dto.ts
@@ -0,0 +1,5 @@
+export class DashboardDataDTO {
+  public chartData1: any;
+  public chartData2: any;
+  public chartData3: any;
+}
