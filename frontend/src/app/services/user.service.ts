41587a642240a8e5c5367b96194408bb39b63006
Adam Resnick <aresnick@rbr-tech.com>
2021-06-02 23:27:48 -0400

    Updated the user.service.ts so it caches the UserInfoDTO observable.

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -3,12 +3,15 @@ import {UserInfoDTO} from "../models/user-info-dto";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
-import {map} from "rxjs/operators";
+import {map, shareReplay} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  // Internal cache of the userInfo object
+  private cachedObservable: Observable<UserInfoDTO> | null = null;
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,11 +21,16 @@ export class UserService {
    */
   public getUserInfo(): Observable<UserInfoDTO> {
 
+    if (this.cachedObservable != null) {
+      // This observable is in the cache.  So, return it from the cache
+      return this.cachedObservable;
+    }
+
     // Construct the URL of the REST call
     const restUrl = environment.baseUrl + '/api/user/me';
 
-    // Return an observable
-    return this.httpClient.get <UserInfoDTO>(restUrl).pipe(
+    // Get the observable and store it in the internal cache
+    this.cachedObservable = this.httpClient.get <UserInfoDTO>(restUrl).pipe(
         map( (userInfoDTO: UserInfoDTO) => {
 
           // Convert the userInfoDTO.pageRoutes into a map
@@ -30,9 +38,14 @@ export class UserService {
           let mapPageRoutes: Map<string, boolean> = new Map(Object.entries(userInfoDTO.pageRoutes));
 
           userInfoDTO.pageRoutes = mapPageRoutes;
+
           return userInfoDTO;
-        }
-      ));
+        }),
+       shareReplay(1)
+      );
+
+    // Return the cached observable
+    return this.cachedObservable;
 
   } // end of getUserInfo
 
